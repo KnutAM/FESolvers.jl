f3734ddf5aead068948518adaa713b4b81330bd5
Knut Andreas Meyer <knutam@gmail.com>
2022-12-13 00:39:43 +0100

    Update examples with linear problem

--- docs/src/literate/plasticity.jl
+++ docs/src/literate/plasticity.jl
@@ -143,10 +143,10 @@ function FESolvers.update_to_next_step!(p::PlasticityProblem, time)
 end;
 
 # Next, we define the updating of the problem given a new guess to the solution. 
-# Note that we use `Δu=nothing` for the case it is not given, to signal no change.
+# Note that we use `Δu::Nothing` for the case it is not given, to signal no change.
 # This version is called directly after update_to_next_step! before entering 
 # the nonlinear iterations. 
-function FESolvers.update_problem!(p::PlasticityProblem, Δu=nothing)
+function FESolvers.update_problem!(p::PlasticityProblem, Δu; kwargs...)
     buf = p.buf 
     def = p.def
     if !isnothing(Δu)
